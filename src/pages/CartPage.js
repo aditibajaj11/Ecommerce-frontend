8fbcbef9551bf9923dd4c07e80b1f6f399501322
aditibajaj11 <128385432+aditibajaj11@users.noreply.github.com>
2024-10-11 17:31:55 +0530

    Update CartPage.js

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -46,7 +46,7 @@ const CartPage = () => {
   // get payment gateway token
   const getToken = async () => {
     try {
-      const { data } = await axios.get("http://localhost:8080/api/v1/product/braintree/token");
+      const { data } = await axios.get("https://ecommerce-backend-enap.onrender.com/api/v1/product/braintree/token");
       setClientToken(data?.clientToken);
     } catch (error) {
       console.log(error);
@@ -65,7 +65,7 @@ const CartPage = () => {
     try {
       setLoading(true);
       const { nonce } = await instance.requestPaymentMethod();
-      const { data } = await axios.post("http://localhost:8080/api/v1/product/braintree/payment", {
+      const { data } = await axios.post("https://ecommerce-backend-enap.onrender.com/api/v1/product/braintree/payment", {
         nonce,
         cart,
       });
@@ -96,7 +96,7 @@ const CartPage = () => {
                   <div className="card flex-row mb-3" key={p._id}>
                     <div className="col-md-4">
                       <img
-                        src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                        src={`https://ecommerce-backend-enap.onrender.com/api/v1/product/product-photo/${p._id}`}
                         className="card-img-top"
                         alt={p.name}
                         width="100%"
